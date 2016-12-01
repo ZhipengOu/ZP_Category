37af18f307ee64755a225cf273433f0687293c35
DamoNorth <18538537502@163.com>
2016-12-01 11:16:13 +0800

    Initial Commit

---
+++ ZP_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZP_Category
+//
+//  Created by administrator on 16/12/1.
+//  Copyright © 2016年 Ozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
